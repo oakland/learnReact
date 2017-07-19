7f53ca01ae72aa8011b311c92eece802c92851fd
jizq <jizhengqistrongman@126.com>
2017-07-19 16:17:44 +0800

    delete src/components/result_item search_result, weather_search files

--- src/components/result_item.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ResultItem = ({weather}) => {
-	return (
-		<li>max: {weather.temp.max}, min: {weather.temp.min}, day: {weather.temp.day}</li>
-	);
-};
-
-export default ResultItem;
